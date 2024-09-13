23d71dd686471d0106a27e2f4a7f987ff80a3a35
anglczbla <angelicaizabella90@gmail.com>
2024-09-13 10:39:52 +0700

    halaman login

--- Korea/Korea/routes/web.php
+++ Korea/Korea/routes/web.php
@@ -1,7 +1,18 @@
-<?php
+<!-- <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');
